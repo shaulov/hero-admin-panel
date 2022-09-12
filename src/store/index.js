3c24615515f1e74b558144a3d7cac29df20336f9
shaulov <shineginn@gmail.com>
2022-09-12 13:18:23 +0300

    Add configureStore from RTK

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,4 @@
-import { combineReducers, createStore, compose, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
+import { configureStore } from '@reduxjs/toolkit';
 import heroesReducer from '../reducers/heroes';
 import filtersReducer from '../reducers/filters';
 
@@ -13,12 +12,10 @@ const sringMiddleWare = (store) => (dispatch) => (action) => {
     return dispatch(action);
 }
 
-const store = createStore(
-    combineReducers({heroes: heroesReducer, filters: filtersReducer}),
-    compose(
-        applyMiddleware(thunk, sringMiddleWare),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-    ),
-);
+const store = configureStore({
+    reducer: {heroesReducer, filtersReducer},
+    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(sringMiddleWare),
+    devTools: process.env.NODE_ENV !== 'production',
+});
 
 export default store;
