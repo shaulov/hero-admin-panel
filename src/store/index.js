18984a132869bcba765190f11c34d8caf223bbcd
shaulov <shineginn@gmail.com>
2022-09-12 12:05:02 +0300

    Add middleware and enhancer

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,40 @@
-import { combineReducers, createStore } from 'redux';
+import { combineReducers, createStore, compose, applyMiddleware } from 'redux';
 import heroesReducer from '../reducers/heroes';
 import filtersReducer from '../reducers/filters';
 
+const sringMiddleWare = (store) => (dispatch) => (action) => {
+    if (typeof action === 'string') {
+        return dispatch({
+            type: action,
+        });
+    }
+
+    return dispatch(action);
+}
+
+const enhancer = (createStore) => (...args) => {
+    const store = createStore(...args);
+    const oldDispatch = store.dispatch;
+
+    store.dispatch = (action) => {
+        if (typeof action === 'string') {
+            return oldDispatch({
+                type: action,
+            });
+        }
+        
+        return oldDispatch(action);
+    }
+
+    return store;
+}
+
 const store = createStore(
     combineReducers({heroes: heroesReducer, filters: filtersReducer}),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+    compose(
+        applyMiddleware(sringMiddleWare),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    ),
+);
 
 export default store;
